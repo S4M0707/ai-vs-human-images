41e8b21c64c47b58a74a7009c9db7d94417f8261
Saransh Kumar <saransh.kumar0707@gmail.com>
2025-04-05 13:46:23 +0530

    adding fastapi model

--- main.py
+++ main.py
@@ -1,42 +1,66 @@
+import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
 from utils.dataset_loader import DatasetLoader
-from utils.model import EfficientNetModel
-from utils.trainer import ModelTrainer
 from utils.predictor import ModelPredictor
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    # Define paths
-    dataset_path = "data"
-    train_csv = "train.csv"
-    test_csv = "test.csv"
+origins = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://localhost:8000",
+    "null"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+dataset_path = "data"
+data_loader = DatasetLoader(dataset_path,  img_size=(224, 224), batch_size=16)
+predictor = ModelPredictor("model/best_model.keras")
+
+@app.post('/upload')
+async def upload(imgFiles: list[UploadFile] = File(...)):
+    imgPaths = []
+
+    for imgFile in imgFiles:
+        imgPath = os.path.join('user_files', f'{imgFile.filename}')
+        with open(imgPath, 'wb') as f:
+            f.write(await imgFile.read())
+        imgPaths.append(imgPath)
+
+    return {'img_paths': imgPaths}
+
+
+@app.post('/predict')
+async def predict(imgPaths: list[str]):
+    try:
+        dataset = data_loader.prepare_image_paths(imgPaths)
+        predictions = predictor.predict(dataset)
+
+        for path in imgPaths:
+            if os.path.exists(path):
+                os.remove(path)
+
+        return {"predictions": predictions.tolist()}
+
+    except Exception as e:
+        for path in imgPaths:
+            if os.path.exists(path):
+                os.remove(path)
+        raise HTTPException(status_code=500, detail=f"Error happened: {str(e)}")
     
-    # Load dataset
-    data_loader = DatasetLoader(dataset_path,  img_size=(224, 224), batch_size=16)
-    train_df = data_loader.load_data(train_csv, "train_data")
-    test_df = data_loader.load_data(test_csv, "test_data_v2")
-
-    # Split data
-    train_df, val_df = train_test_split(train_df, test_size=0.2, stratify=train_df['label'], random_state=42)
-
-    # Prepare datasets
-    train_dataset = data_loader.prepare_dataset(train_df, is_training=True)
-    val_dataset = data_loader.prepare_dataset(val_df, is_training=False)
-
-    # Create model
-    model_builder = EfficientNetModel(num_classes=train_df['label'].nunique())
-    model = model_builder.model
-    model_builder.compile_model()
-
-    # Train model
-    trainer = ModelTrainer(model, train_dataset, val_dataset)
-    # trainer.train(epochs=20)
-
-    # Load trained model and evaluate
-    predictor = ModelPredictor("model/best_model.keras")
-    predictions = predictor.predict(val_dataset)
-    print(predictions)
-    predictor.evaluate(pd.get_dummies(val_df["label"]).values, predictions)
-    predictor.plot_roc_curve(pd.get_dummies(val_df["label"]).values, predictions)
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- utils/dataset_loader.py
+++ utils/dataset_loader.py
@@ -16,17 +16,38 @@ class DatasetLoader:
         df["file_name"] = df["file_name"].apply(lambda x: os.path.join(self.dataset_path, image_folder, os.path.basename(x)))
         return df
 
-    def preprocess_image(self, image_path, label=None):
+    def _load_and_preprocess(self, image_path):
         image = tf.io.read_file(image_path)
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, self.img_size)
         image = tf.image.convert_image_dtype(image, tf.float32)
-        return (image, label) if label is not None else image
+        return image
+    
+    def preprocess_image_with_label(self, path, label):
+        image = self._load_and_preprocess(path)
+        return image, label
 
+    def preprocess_image_without_label(self, path):
+        image = self._load_and_preprocess(path)
+        return image
+    
     def prepare_dataset(self, df, is_training=True):
-        paths, labels = df["file_name"].values, pd.get_dummies(df["label"]).astype(int).values
-        dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
-        dataset = dataset.map(self.preprocess_image, num_parallel_calls=self.autotune)
+        paths = df["file_name"].values
+
         if is_training:
+            labels = pd.get_dummies(df["label"]).astype(int).values
+            dataset = tf.data.Dataset.from_tensor_slices((paths, labels))
+            dataset = dataset.map(self.preprocess_image_with_label, num_parallel_calls=self.autotune)
             dataset = dataset.shuffle(1000)
+        else:
+            dataset = tf.data.Dataset.from_tensor_slices(paths)
+            dataset = dataset.map(self.preprocess_image_without_label, num_parallel_calls=self.autotune)
+
+        return dataset.batch(self.batch_size).prefetch(self.autotune)
+    
+    def prepare_image_paths(self, image_paths):
+        if isinstance(image_paths, str):
+            image_paths = [image_paths]
+        dataset = tf.data.Dataset.from_tensor_slices(image_paths)
+        dataset = dataset.map(self.preprocess_image_without_label, num_parallel_calls=self.autotune)
         return dataset.batch(self.batch_size).prefetch(self.autotune)
